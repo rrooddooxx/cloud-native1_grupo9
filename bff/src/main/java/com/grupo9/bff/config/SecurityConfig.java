c0758960f68d698666eaedc5e2314b2cdf329573
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-06-10 18:31:48 -0400

    AD B2C working

--- bff/src/main/java/com/grupo9/bff/config/SecurityConfig.java
+++ bff/src/main/java/com/grupo9/bff/config/SecurityConfig.java
@@ -1,32 +1,27 @@
 package com.grupo9.bff.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri;
-
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation(issuerUri);
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/private/**").authenticated().anyRequest().permitAll())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .authorizeHttpRequests(authz -> authz
+                                               .requestMatchers("/public/**").permitAll()
+                                               .anyRequest().authenticated()
+                                      )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                                              .jwt(Customizer.withDefaults())
+                                     );
+
         return http.build();
     }
 }
