63df49662853feadb7dcdffcd4f1398c51e5a809
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-06-09 22:25:50 -0400

    primer commit

---
+++ backend-cn1-grupo9/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "cn1-grupo9"
---
+++ backend-cn1-grupo9/src/test/java/com/skravetz/cn1grupo9/Cn1Grupo9ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.skravetz.cn1grupo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cn1Grupo9ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
