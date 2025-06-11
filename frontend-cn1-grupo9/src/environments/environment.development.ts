b09c62cb61a298c3515ff6bf513babc368e6ec2e
Manuel <manuel.bastias>
2025-06-10 22:50:47 -0400

    feat: home, dashboard an products

--- frontend-cn1-grupo9/src/environments/environment.development.ts
+++ frontend-cn1-grupo9/src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
-    useMockData: true,
+    useMockData: false,
     apiUrl: 'http://localhost:8080/api',
+    bffUrl: 'http://localhost:8080',
     msalConfig: {
         auth: {
             clientId: '59d93efc-d6d0-44a2-8107-35a3315120f9',
