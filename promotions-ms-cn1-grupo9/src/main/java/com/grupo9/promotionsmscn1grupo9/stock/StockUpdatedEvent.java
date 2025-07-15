764175629abbeb5b9137b211882c7868418b7e58
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-07-14 21:58:58 -0400

    fix kafka events

--- promotions-ms-cn1-grupo9/src/main/java/com/grupo9/promotionsmscn1grupo9/stock/StockUpdatedEvent.java
+++ promotions-ms-cn1-grupo9/src/main/java/com/grupo9/promotionsmscn1grupo9/stock/StockUpdatedEvent.java
@@ -1,10 +1,14 @@
 package com.grupo9.promotionsmscn1grupo9.stock;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockUpdatedEvent {
   private Integer productId;
   private Integer quantity;
