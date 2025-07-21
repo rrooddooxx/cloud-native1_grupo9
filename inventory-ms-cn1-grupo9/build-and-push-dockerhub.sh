f3ecc2e265bf82cba6286d4abbfc4a9c582e13eb
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-07-20 23:58:18 -0400

    updates

--- inventory-ms-cn1-grupo9/build-and-push-dockerhub.sh
+++ inventory-ms-cn1-grupo9/build-and-push-dockerhub.sh
@@ -9,9 +9,10 @@ IMAGE_TAG="latest"
 FULL_IMAGE_NAME="$DOCKERHUB_USERNAME/$APP_NAME:$IMAGE_TAG"
 
 echo "🛠️ Building multi-arch Docker image (linux/amd64)..."
-docker buildx create --use --name multiarch-builder || true
+docker buildx create --use --name amd64-builder || true
 docker buildx build \
-  --platform linux/amd64 \
+  --platform=linux/amd64 \
+  --builder=amd64-builder \
   -t $FULL_IMAGE_NAME \
   . \
   --push
