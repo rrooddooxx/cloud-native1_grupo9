bfede8dc72a5656a36c1a0fea616cdfc47392487
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-07-21 03:17:20 -0400

    update kafka config

--- frontend-cn1-grupo9/src/environments/environment.development.ts
+++ frontend-cn1-grupo9/src/environments/environment.development.ts
@@ -3,7 +3,8 @@ export const environment = {
     useMockData: false,
     apiUrl: 'http://localhost:8080/api',
     // bffUrl: 'https://kegbnhdml7.execute-api.us-east-1.amazonaws.com/production',
-    bffUrl: 'http://localhost:8080/api',
+    // bffUrl: 'http://localhost:8080/api',
+    bffUrl: 'https://v9l4qmcvla.execute-api.us-east-1.amazonaws.com/prod/bff/api',
     msalConfig: {
         auth: {
             clientId: '59d93efc-d6d0-44a2-8107-35a3315120f9',
--- frontend-cn1-grupo9/src/environments/environment.ts
+++ frontend-cn1-grupo9/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     useMockData: false,
     apiUrl: 'http://localhost:8080/api',
-    bffUrl: 'http://localhost:8080/api',
+    bffUrl: 'https://v9l4qmcvla.execute-api.us-east-1.amazonaws.com/prod/bff/api',
     msalConfig: {
         auth: {
             clientId: '59d93efc-d6d0-44a2-8107-35a3315120f9',
